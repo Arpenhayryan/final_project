ab99d89813dee81d4eecb2151e0deaec3e989ecd
Arpenhayryan <arpenik_hayryan@edu.aua.am>
2017-09-23 19:37:28 +0400

    Add files via upload

---
+++ homework3/homework3.js
@@ -0,0 +1,80 @@
+//1
+   const myFunc = function(a,b,c,d){
+	return a+b*c-d;
+   };
+  myFunc (1,2, 49, 11);
+
+
+ //2
+   const haha = function(){
+    return 1; 
+   }; 
+ 
+
+ //3
+const myFunction=function(a,b){
+  console.log(a+b);
+	};
+myFunction (1,2);
+
+
+ //4 
+ const firstName = function () {
+ 	return 'Afric';
+ };
+const lastName = function (){
+	return 'Simone';
+};
+const fullName = function (name, surname){
+returns name + ' ' + surname
+};
+fullName (firstName(), lastName());
+
+
+//5
+const myFunction=function(){
+  const myStrings =('Jamaica', 'Wolverine', 'onion');
+  const result=console.log(myStrings.length);
+  return result;
+};
+
+
+//6
+const firstTruthy=function(cat, 45, haha){
+      const truthy=cat || 45 || haha;
+      return truthy;
+};
+
+
+//7
+const Equal=function(num1,num2){
+
+		if (num1===num2) {
+			return 0;
+		}
+		else if(num1>num2){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	}; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
